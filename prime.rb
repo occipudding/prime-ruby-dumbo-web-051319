0d9ee7b9d4b44793ccbe6b0439e230ab3a5f3f1a
Joshua Suskin <jsuskin85@gmail.com>
2019-05-06 08:44:08 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,16 @@
 def prime?(num)
+  arr = []
   if num < 0
     return false
   end
   if num <= 2
     return true
   end
-  (2..(num / 2)).each |x|
-    if num % x == 0
+  (2..(num / 2)).each |x| arr << x
+  arr.each |n| {
+    if num % n == 0
       return false
     end
+  }
   true
 end
