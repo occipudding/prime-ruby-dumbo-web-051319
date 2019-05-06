958e643fea1285e63f358505fe7d901ce0659675
Joshua Suskin <jsuskin85@gmail.com>
2019-05-06 08:38:01 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,10 @@ def prime?(num)
   if num <= 2
     return true
   end
-  if (num % (2..(num / 2).to_i) == 0)
-    return false
-  end
+  (2..(num / 2)).each |x| {
+    if num % x == 0
+      return false
+    end
+  }
   true
 end
