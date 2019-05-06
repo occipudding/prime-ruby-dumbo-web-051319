bda6a476dfb047c7051a8da0e78dd5cee4c51fd9
Joshua Suskin <jsuskin85@gmail.com>
2019-05-06 08:35:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 def prime?(num)
   range = 2..(num / 2)
   if num <= 2
-    return false
-  end
-  if (num % (2..(num / 2)) == 0)
     return true
   end
-  false
+  if (num % (2..(num / 2)) == 0)
+    return false
+  end
+  true
 end
